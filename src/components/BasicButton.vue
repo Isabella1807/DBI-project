fb7512a50b88c4573b77c64527fec2a5410ad986
cloud14 <kontaktclaudia@gmail.com>
2025-03-28 10:07:48 +0100

    BasicButton er i fuld sving

--- src/components/BasicButton.vue
+++ src/components/BasicButton.vue
@@ -1,12 +1,43 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-
-
+    <button class="basicButton">
+        <p>Opret</p>
+        <span class="plusIcon">+</span>
+    </button>
 </template>
+
+<script setup>
+</script>
 
 <style scoped lang="scss">
 
+p {
+    @include buttonText;
+    text-transform: uppercase;
+}
+
+.basicButton {
+    background-color: $black;
+    color: $white;
+    border: none;
+    padding: 20px 32px;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    cursor: pointer;
+    transition: background-color 0.2s ease-in-out;
+
+    @include buttonText;
+
+    &:hover {
+        background-color: $mediumGreen;
+    }
+
+    &:active {
+        background-color: $darkGreen;
+    }
+}
+
+.plusIcon {
+    font-size: 16px;
+}
 </style>
--- src/views/testView.vue
+++ src/views/testView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import TestComp from "@/components/TestComp.vue";
+import BasicButton from "@/components/BasicButton.vue";
 </script>
 
 <template>
-<div>
-<TestComp/>
-</div>
+    <div>
+        <TestComp />
+        <BasicButton />
+    </div>
 </template>
 
 <style scoped lang="scss">
