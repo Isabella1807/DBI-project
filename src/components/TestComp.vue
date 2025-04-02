45801e69ec6eb60bab85fbbf53ed4128f903984e
Isabella1807 <megaoreomuffin@hotmail.com>
2025-04-02 17:18:17 +0200

    Created icon and tab component

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
 
+import BasicIcon from '@/components/BasicIcon.vue';
+import TabItem from '@/components/TabItem.vue';
 </script>
 
 <template>
 <h1>HEJSA</h1>
+  <BasicIcon name="Qr_Code" selected/>
+  <BasicIcon name="Qr_Code" large/>
+
+  <TabItem icon-name="Qr_Code" label="Enheder"/>
+  <TabItem icon-name="Qr_Code" label="Enheder" selected/>
 </template>
 
 <style scoped lang="scss">
 h1{
-  
   @include heading1;
-
 }
 </style>
