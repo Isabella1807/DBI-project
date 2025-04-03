7449dd4f31ee9ddfed8d63f44c4b36f023b4f1f1
Isabella1807 <megaoreomuffin@hotmail.com>
2025-04-03 10:32:14 +0200

    removed stroke-width from svg file

--- src/components/BasicIcon.vue
+++ src/components/BasicIcon.vue
@@ -33,6 +33,7 @@ const filePath = computed(() => {
 svg {
   display: inline-block;
   stroke-width: 2;
+  color: red;
   &.selected{
     stroke-width: 2.5;
   }
--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -8,9 +8,11 @@ import TabItem from '@/components/TabItem.vue';
 <h1>HEJSA</h1>
   <BasicIcon name="Qr_Code" selected/>
   <BasicIcon name="Qr_Code" large/>
+  <BasicIcon name="Notebook" small/>
 
   <TabItem icon-name="Qr_Code" label="Enheder"/>
   <TabItem icon-name="Qr_Code" label="Enheder" selected/>
+  <TabItem icon-name="" label="hkkjkjjk"/>
 </template>
 
 <style scoped lang="scss">
