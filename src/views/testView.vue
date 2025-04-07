eca0fb0956966ce00e1cba8ae691a3fd30bd7e7c
AndersBuch <119923346+AndersBuch@users.noreply.github.com>
2025-04-07 13:45:13 +0200

    added FolderSection comp

--- src/views/testView.vue
+++ src/views/testView.vue
@@ -2,6 +2,7 @@
 import TestComp from "@/components/TestComp.vue";
 import BasicButton from "@/components/BasicButton.vue";
 import FolderComponet from '@/components/folderComponet.vue';
+import FolderSection from "@/components/FolderSection.vue";
 </script>
 
 <template>
@@ -11,7 +12,8 @@ import FolderComponet from '@/components/folderComponet.vue';
         <BasicButton label="Eksporter" type="secondary" :showIcon="false" />
         <BasicButton label="Annuller" type="annuller" :showIcon="false" />
         <BasicButton label="Ja - afslut og luk ned" type="lukNed" :showIcon="false" />
-        <FolderComponet />
+        <FolderComponet/>
+        <FolderSection view="list"/>
     </div>
 </template>
 
