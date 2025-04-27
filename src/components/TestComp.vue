c83d7c2d3cbb50acad20fdf6b470426974c340a7
Isabella1807 <megaoreomuffin@hotmail.com>
2025-04-27 16:44:55 +0200

    fixed testcomp gitflow error

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-
 import BasicIcon from '@/components/atoms/BasicIcon.vue';
-import TabItem from '@/components/molecules/TabItem.vue';
+//import TabItem from '@/components/molecules/TabItem.vue';
 </script>
 
 <template>
@@ -10,8 +9,8 @@ import TabItem from '@/components/molecules/TabItem.vue';
   <BasicIcon name="Qr_Code" large/>
   <BasicIcon name="Notebook" small/>
 
-  <TabItem icon-name="Qr_Code" label="Enheder"/>
-  <TabItem icon-name="Qr_Code" label="Enheder" selected/>
+  <!--  <TabItem icon-name="Qr_Code" label="Enheder"/>
+    <TabItem icon-name="Qr_Code" label="Enheder" selected/>-->
   <!--  <TabItem icon-name="" label="hkkjkjjk"/>-->
 </template>
 
