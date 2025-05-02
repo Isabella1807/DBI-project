a4ea1d723910af4bf451cb0202b85606fabdc394
Isabella1807 <megaoreomuffin@hotmail.com>
2025-05-02 16:45:07 +0200

    added eslint workflow to dev and main

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'eslint/config';
+import {defineConfig} from 'eslint/config';
 import globals from 'globals';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
@@ -6,33 +6,31 @@ import pluginVue from 'eslint-plugin-vue';
 
 
 export default defineConfig([
-  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
-  { files: ['**/*.{js,mjs,cjs,ts,vue}'], languageOptions: { globals: globals.browser } },
-  { files: ['**/*.{js,mjs,cjs,ts,vue}'], plugins: { js }, extends: ['js/recommended'] },
+  {files: ['**/*.{js,mjs,cjs,ts,vue}']},
+  {files: ['**/*.{js,mjs,cjs,ts,vue}'], languageOptions: {globals: globals.browser}},
+  {files: ['**/*.{js,mjs,cjs,ts,vue}'], plugins: {js}, extends: ['js/recommended']},
   tseslint.configs.recommended,
   pluginVue.configs['flat/essential'],
-  { files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+  {files: ['**/*.vue'], languageOptions: {parserOptions: {parser: tseslint.parser}}},
   {
     rules: {
-        //'vue/component-tags-order': ['error', {
-           // order: ['script', 'template', 'style']
-       // }],
-        'arrow-spacing': ['error', { 'before': true, 'after': true }],
-        'brace-style': ['error', '1tbs'],
-        'comma-dangle': ['error', 'always-multiline'],
-        //'line-comment-position': ['error', 'always'],
-        //'array-bracket-newline': ['error', 'consistent'],
-        'eqeqeq': ['error', 'always'],
-        'indent': ['error', 2],
-        'no-eval': ['error'],
-        //'no-trailing-spaces': ['error'],
-        'no-unused-vars': ['error'],
-        'no-var': ['error'],
-        'prefer-const': ['error'],
-        'quotes': ['error', 'single'],
-        'semi': ['error', 'always'],
-        'keyword-spacing': ["error", { "before": true }],
+      'arrow-spacing': ['error', {'before': true, 'after': true}],
+      'brace-style': ['error', '1tbs'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'eqeqeq': ['error', 'always'],
+      'indent': ['error', 2],
+      'no-eval': ['error'],
+      'no-unused-vars': ['error'],
+      'no-var': ['error'],
+      'prefer-const': ['error'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'keyword-spacing': ["error", {"before": true}],
+      'no-console': 'warn',
+      //'line-comment-position': ['error', 'always'],
+      //'array-bracket-newline': ['error', 'consistent'],
+      //'no-trailing-spaces': ['error'],
     }
-}
+  }
 ]);
 
