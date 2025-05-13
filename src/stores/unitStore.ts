5e4ac63c2c9cc3eb97a72c0b21958ef9ab6a3f79
Isabella1807 <megaoreomuffin@hotmail.com>
2025-05-13 20:26:19 +0200

    Added 'create unit' in unit store and unit service layer

--- src/stores/unitStore.ts
+++ src/stores/unitStore.ts
@@ -1,6 +1,31 @@
 import {defineStore} from 'pinia';
+import type {unitInputType, UnitTypeWithId} from '@/types/unitTypes.ts';
+import {useFolderStore} from '@/stores/folderStore.ts';
+import {createUnit} from '@/services/unitService.ts';
 
 export const useUnitStore = defineStore('unitStore', () => {
 
-  return {};
+  const visibleUnits: UnitTypeWithId[] = [];
+
+  const createNew = (unit: unitInputType) => {
+    //console.log(unit);
+
+    const folderStore = useFolderStore();
+    //console.log(folderStore.currentFolderName, folderStore.currentFolderId);
+
+    createUnit({
+      ...unit,
+      parentId: folderStore.currentFolderId,
+    }).then(createdUnit => {
+      console.log(createdUnit);
+      visibleUnits.push(createdUnit);
+    }).catch(error => {
+      console.log(error);
+    });
+  };
+
+  return {
+    createNew,
+    visibleUnits
+  };
 });
--- src/types/unitTypes.ts
+++ src/types/unitTypes.ts
@@ -2,9 +2,15 @@ export interface BaseUnitType {
   name: string;
   description: string;
   syncId: string;
-  parentId: string;
+  parentId: string | null;
 }
 
 export interface UnitTypeWithId extends BaseUnitType {
   id: string;
+}
+
+export interface unitInputType {
+  name: string;
+  description: string;
+  syncId: string;
 }
